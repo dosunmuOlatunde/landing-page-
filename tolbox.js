a9595376fcb48cd47e864c8047e1998566c033d2
dosunmuOlatunde <yusufola155@gmail.com>
2026-05-27 22:55:49 +0100

    Add files via upload

---
+++ tolbox.js
@@ -0,0 +1,11 @@
+
+document.getElementById("menu-bar").addEventListener("click",()=>{
+    document.getElementById("navbar").classList.toggle("active")
+
+   
+})
+
+document.getElementById("menu-bar").addEventListener("click",()=>{
+    document.getElementById("menu-bar").classList.toggle("fa-bars");
+    document.getElementById("menu-bar").classList.toggle("fa-x");
+})
